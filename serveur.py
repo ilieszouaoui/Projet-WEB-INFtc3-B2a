5d3f337eff767bd4f430f1442964eb72eb5ad425
Dimitri <dimitri.lefoll31@gmail.com>
2020-06-15 18:48:20 +0200

    changements du code du serveur pour s'adapter au client

--- serveur.py
+++ serveur.py
@@ -27,19 +27,15 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
   def do_GET(self):
     # on récupère les paramètres
     self.init_params()
-
-    # le chemin d'accès commence par /time
-    if self.path.startswith('/time'):
-      self.send_time()
-   
-    # le chemin d'accès commence par /countries
-    elif self.path.startswith('/countries'):
-      self.send_countries()
-
-    # le chemin d'accès commence par /country et se poursuit par un nom de pays
-    elif self.path_info[0] == 'country' and len(self.path_info) > 1:
-      self.send_country(self.path_info[1])
       
+    # le chemin d'accès commence par /service/countries/...
+    if self.path_info[0] == 'service' and self.path_info[1] == 'countries' and len(self.path_info) > 1:
+      self.send_json_countries()
+    
+    # le chemin d'accès commence par /service/country/...
+    elif self.path_info[0] == 'service' and self.path_info[1] == 'country' and len(self.path_info) > 2:
+      self.send_json_country(self.path_info[2])
+    
     # ou pas...
     else:
       self.send_static()
@@ -91,110 +87,70 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     print('body =',length,ctype,self.body)
     print('params =', self.params)
 
+  def send_json_countries(self):
+    # on récupère la liste de pays depuis la base de données
+    r = self.db_get_countries()
 
-  #
-  # On envoie un document avec l'heure
-  #
-  def send_time(self):
+    # on renvoie une liste de dictionnaires au format JSON
+    data = [ {k:a[k] for k in a.keys()} for a in r]
+    json_data = json.dumps(data, indent=4)
+    headers = [('Content-Type','application/json')]
+    print(json_data)
+    self.send(json_data,headers)
+       
+  
+  def send_json_country(self,country):
+
+    # on récupère le pays depuis la base de données
+    r = self.db_get_country(country)
+
+    # on n'a pas trouvé le pays demandé
+    if r == None:
+      self.send_error(404,'Country not found')
+
+    # on renvoie un dictionnaire au format JSON
+    else:
+      data = {k:r[k] for k in r.keys()}
+      json_data = json.dumps(data, indent=4)
+      headers = [('Content-Type','application/json')]
+      self.send(json_data,headers)
     
-    # on récupère l'heure
-    time = self.date_time_string()
-
-    # on génère un document au format html
-    body = '<!doctype html>' + \
-           '<meta charset="utf-8">' + \
-           '<title>l\'heure</title>' + \
-           '<div>Voici l\'heure du serveur :</div>' + \
-           '<pre>{}</pre>'.format(time)
-
-    # pour prévenir qu'il s'agit d'une ressource au format html
-    headers = [('Content-Type','text/html;charset=utf-8')]
-
-    # on envoie
-    self.send(body,headers)
-
-  #
-  # On renvoie la liste des pays
-  #
-  def send_countries(self):
-
-    # création d'un curseur (conn est globale)
+  def db_get_countries(self):
     c = conn.cursor()
-    
-    # récupération de la liste des pays dans la base
-    c.execute("SELECT name FROM countries")
-    r = c.fetchall()
+    sql = 'SELECT wp, capital, latitude, longitude from countries'
+    c.execute(sql)
+    return c.fetchall()
 
-    # construction de la réponse
-    txt = 'List of all {} countries :\n'.format(len(r))
-    n = 0
-    for a in r:
-       n += 1
-       txt = txt + '[{}] - {}\n'.format(n,a[0])
-    
-    # envoi de la réponse
-    headers = [('Content-Type','text/plain;charset=utf-8')]
-    self.send(txt,headers)
-
-  #
-  # On renvoie les informations d'un pays
-  #
-  def send_country(self,country):
-
+  def db_get_country(self,country):
     # préparation de la requête SQL
     c = conn.cursor()
     sql = 'SELECT * from countries WHERE wp=?'
 
     # récupération de l'information (ou pas)
     c.execute(sql,(country,))
-    r = c.fetchone()
+    return c.fetchone()      
 
-    # on n'a pas trouvé le pays demandé
-    if r == None:
-      self.send_error(404,'Country not found')
-
-    # on génère un document au format html
-    else:
-      body = '<!DOCTYPE html>\n<meta charset="utf-8">\n'
-      body += '<title>{}</title>'.format(country)
-      body += '<link rel="stylesheet" href="/ProjetWeb.css">'
-      body += '<main>'
-      body += '<h1>{}</h1>'.format(r['name'])
-      body += '<ul>'
-      body += '<li>{}: {}</li>'.format('Leader',r['leader'])
-      body += '<li>{}: {} km2</li>'.format('Superficie',r['area_km2'])
-      body += '<li>{}: {}</li>'.format('Capital',r['capital'])
-      body += '<li>{}: {:.3f}</li>'.format('Latitude',r['latitude'])
-      body += '<li>{}: {:.3f}</li>'.format('Longitude',r['longitude'])
-      body += '<li>{}: {:.3f}</li>'.format('Coefficient de Gini',r['gini'])
-      body += '<li>{}: {:.3f}</li>'.format('Indice de developpement humain (IDH)',r['hdi'])
-      body += '</ul>'
-      body += '</main>'
-
-      # on envoie la réponse
-      headers = [('Content-Type','text/html;charset=utf-8')]
-      self.send(body,headers)
-
-  #
   # On envoie les entêtes et le corps fourni
-  #
   def send(self,body,headers=[]):
 
     # on encode la chaine de caractères à envoyer
     encoded = bytes(body, 'UTF-8')
 
+    self.send_raw(encoded,headers)
+
+
+  def send_raw(self,data,headers=[]):
     # on envoie la ligne de statut
     self.send_response(200)
 
     # on envoie les lignes d'entête et la ligne vide
     [self.send_header(*t) for t in headers]
-    self.send_header('Content-Length',int(len(encoded)))
+    self.send_header('Content-Length',int(len(data)))
     self.end_headers()
 
     # on envoie le corps de la réponse
-    self.wfile.write(encoded)
-
- 
+    self.wfile.write(data)
+    
 #
 # Ouverture d'une connexion avec la base de données
 #
